efa326bd10e8aa923566c28b4e7a2b9227d740cf
Chris Bucknell <99325130+C-Bucknell@users.noreply.github.com>
2025-08-21 16:00:21 +1000

    Update task1_test.py

--- task1_test.py
+++ task1_test.py
@@ -1,4 +1,15 @@
-import task1;
+import os.path
+import sys
+from task1 import main
+from tud_tests import *
 
-def test_hello():
+def test_task1():
+    try:
+        exists = os.path.exists("task1.py")
+        assert exist == True
+    except:
+        sys.exit()
+
+    set_keyboard_input(["Bob"])
+    
     assert hello.hello_world() == "Hello World!"
